3e2d402d1f1c24ed1fb315b525732ce37c783ad4
Piotr Buława <piotrbulawa88@gmail.com>
2022-06-14 18:09:23 +0200

    add alert popup

--- src/js/main.js
+++ src/js/main.js
@@ -3,42 +3,46 @@ const selectButton = document.querySelector('.open__button')
 const selectModelPopup = document.querySelector('.model__popup')
 const selectContainerPopup = document.querySelector('.container__popup')
 const selectModelTextAlert = document.getElementById('textAlert')
-const selectModelOverlay = document.querySelector('#overlay')
+const selectModelOverlay = document.querySelector('.overlay')
 const selectCloseButton = document.querySelector('span')
 const selectRestartButton = document.querySelector('.restart__Button')
 
-
-//function counter + text
+//counter
 let counterButton = 1;
 
+//function counter + text
 const counter = () => {
-  selectModelTextAlert.innerText = `You have click ${counterButton++} times to relate butto`
-  if (counterButton > 5) {
-    selectRestartButton.style.display = 'block'
+selectModelTextAlert.textContent = `You have click ${counterButton++} times to relate button`
+
+  if (counterButton >= 5) {
+   selectRestartButton.style.display = 'block'
   }
+ 
 }
 
 // function show alert
 const openPopup = () => {
-  selectModelPopup.classList.add('active');
-  selectModelOverlay.classList.add('active');
+  selectModelPopup.classList.add('active'); 
+     selectModelOverlay.classList.add('active');
   counter()
 }
 
 //function close popup
 const closePopup = () => {
   selectModelPopup.classList.remove('active');
-  selectModelOverlay.classList.remove('active');
+selectModelOverlay.classList.remove('active');
 }
 
 //Reset counter
 const resetCounter = () => {
   counterButton = 1;
-  selectModelTextAlert.innerText = `You have click ${counterButton++} times to relate butto`
+  selectModelTextAlert.innerText = `You have click ${counterButton} times to relate butto`
   selectRestartButton.style.display = 'none'
 }
 
 //Listen 
+selectModelPopup.addEventListener('click', closePopup)
 selectButton.addEventListener('click', openPopup)
 selectCloseButton.addEventListener('click', closePopup)
 selectRestartButton.addEventListener('click', resetCounter)
+
