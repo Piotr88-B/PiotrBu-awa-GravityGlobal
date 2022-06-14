e57e3aa171ffe01f91f3522ce1c70534dc758835
Piotr Buława <piotrbulawa88@gmail.com>
2022-06-14 18:30:12 +0200

    add smalle amendments

--- src/js/main.js
+++ src/js/main.js
@@ -1,37 +1,36 @@
 // get element
 const selectButton = document.querySelector('.open__button')
 const selectModelPopup = document.querySelector('.model__popup')
-const selectContainerPopup = document.querySelector('.container__popup')
 const selectModelTextAlert = document.getElementById('textAlert')
 const selectModelOverlay = document.querySelector('.overlay')
 const selectCloseButton = document.querySelector('span')
 const selectRestartButton = document.querySelector('.restart__Button')
 
 //counter
- let counterButton = 1;
+let counterButton = 1;
 
 //function counter + text
 const counter = () => {
-let counterButton = localStorage.counterButton ++
-selectModelTextAlert.textContent = `You have click ${counterButton} times to relate button`
+  let counterButton = localStorage.counterButton++
+  selectModelTextAlert.textContent = `You have click ${counterButton} times to relate button`
 
   if (counterButton >= 5) {
-   selectRestartButton.style.display = 'block'
+    selectRestartButton.style.display = 'block'
   }
- 
+
 }
 
 // function show alert
 const openPopup = () => {
-  selectModelPopup.classList.add('active'); 
-     selectModelOverlay.classList.add('active');
+  selectModelPopup.classList.add('active');
+  selectModelOverlay.classList.add('active');
   counter()
 }
 
 //function close popup
 const closePopup = () => {
   selectModelPopup.classList.remove('active');
-selectModelOverlay.classList.remove('active');
+  selectModelOverlay.classList.remove('active');
 }
 
 //Reset counter
@@ -46,4 +45,3 @@ selectModelPopup.addEventListener('click', closePopup)
 selectButton.addEventListener('click', openPopup)
 selectCloseButton.addEventListener('click', closePopup)
 selectRestartButton.addEventListener('click', resetCounter)
-
